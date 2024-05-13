9be2b46677a938adaf266392494b742367ecf77c
Yuvi dew <127539111+yuvidew@users.noreply.github.com>
2024-05-13 23:02:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Counter App</h1>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    )
+  }
+
+}
+
+
+export default App;
